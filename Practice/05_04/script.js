62196309e1a23ffb9d9b53df0f51ccac5da408d8
Thalia <148969711+TZevs@users.noreply.github.com>
2024-07-03 09:32:03 +0100

    Yesterday's work

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,13 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+const backpackOne = document.querySelector("article");
+console.log("The First BackPack:", backpackOne);
+
+const backpackTwo = document.querySelector("article:last-child");
+console.log("The Second BackPack:", backpackTwo);
+
+document.querySelectorAll(".backpack:last-child li span").forEach(item => item.style.color = "green");
+document.getElementById("pack01").querySelectorAll("li span").forEach(item => item.style.backgroundColor = "lightblue");
+
